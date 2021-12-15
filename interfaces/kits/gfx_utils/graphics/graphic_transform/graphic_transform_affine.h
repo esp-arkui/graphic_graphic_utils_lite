a719cc988bb8e99378dc1c846c84a7632258e6e6
xvcheng1979101 <xvcheng1979@qq.com>
2021-12-15 15:28:08 +0800

    代码修改

--- interfaces/kits/gfx_utils/graphics/graphic_transform/graphic_transform_affine.h
+++ interfaces/kits/gfx_utils/graphics/graphic_transform/graphic_transform_affine.h
@@ -34,14 +34,17 @@ namespace OHOS {
          * @since 1.0
          * @version 1.0
          */
-        TransAffine() : scaleX(1.0), shearY(0.0), shearX(0.0), scaleY(1.0), translateX(0.0), translateY(0.0) {}
+        TransAffine() :
+            scaleX(1.0), shearY(0.0), shearX(0.0), scaleY(1.0), translateX(0.0), translateY(0.0)
+        {}
         /**
          * @brief 自定义矩阵
          * @since 1.0
          * @version 1.0
          */
-        TransAffine(double v0, double v1, double v2, double v3, double v4, double v5)
-            : scaleX(v0), shearY(v1), shearX(v2), scaleY(v3), translateX(v4), translateY(v5) {}
+        TransAffine(double v0, double v1, double v2, double v3, double v4, double v5) :
+            scaleX(v0), shearY(v1), shearX(v2), scaleY(v3), translateX(v4), translateY(v5)
+        {}
         /**
          * @brief 用于将矩形转换为平行四边形
          * @since 1.0
@@ -95,7 +98,7 @@ namespace OHOS {
          * @since 1.0
          * @version 1.0
          */
-        const TransAffine& Rotate(double a);
+        const TransAffine& Rotate(double angle);
         /**
          * @brief 图元放大
          * @param scale 整体放大系数
@@ -127,7 +130,7 @@ namespace OHOS {
          */
         const TransAffine& Invert();
 
-        const TransAffine& operator *= (const TransAffine& metrix)
+        const TransAffine& operator*=(const TransAffine& metrix)
         {
             return Multiply(metrix);
         }
@@ -259,8 +262,8 @@ namespace OHOS {
      */
     class TransAffineRotation : public TransAffine {
     public:
-        TransAffineRotation(double angle)
-            : TransAffine(std::cos(angle), std::sin(angle), -std::sin(angle), std::cos(angle), 0.0, 0.0)
+        TransAffineRotation(double angle) :
+            TransAffine(std::cos(angle), std::sin(angle), -std::sin(angle), std::cos(angle), 0.0, 0.0)
         {}
     };
 
@@ -271,10 +274,12 @@ namespace OHOS {
      */
     class TransAffineScaling : public TransAffine {
     public:
-        TransAffineScaling(double x, double y) : TransAffine(x, 0.0, 0.0, y, 0.0, 0.0)
+        TransAffineScaling(double x, double y) :
+            TransAffine(x, 0.0, 0.0, y, 0.0, 0.0)
         {}
 
-        TransAffineScaling(double scale) : TransAffine(scale, 0.0, 0.0, scale, 0.0, 0.0)
+        TransAffineScaling(double scale) :
+            TransAffine(scale, 0.0, 0.0, scale, 0.0, 0.0)
         {}
     };
     /**
@@ -284,7 +289,8 @@ namespace OHOS {
      */
     class TransAffineTranslation : public TransAffine {
     public:
-        TransAffineTranslation(double x, double y) : TransAffine(1.0, 0.0, 0.0, 1.0, x, y)
+        TransAffineTranslation(double x, double y) :
+            TransAffine(1.0, 0.0, 0.0, 1.0, x, y)
         {}
     };
 } // namespace OHOS
