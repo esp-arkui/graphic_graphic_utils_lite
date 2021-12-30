d67234240f5588e4b5bd709b7712e3271f386a78
xvcheng1979101 <xvcheng1979@qq.com>
2021-12-30 11:44:09 +0800

    代码提交

--- interfaces/innerkits/graphic_neon_pipeline.h
+++ interfaces/innerkits/graphic_neon_pipeline.h
@@ -18,168 +18,207 @@
 
 #include "graphic_config.h"
 #ifdef ARM_NEON_OPT
-#include <arm_neon.h>
-#include "gfx_utils/color.h"
-#include "graphic_neon_utils.h"
+#    include <arm_neon.h>
+
+#    include "gfx_utils/color.h"
+#    include "graphic_neon_utils.h"
 
 namespace OHOS {
-using LoadBuf   = void (*)(uint8_t* buf, uint8x8_t& r, uint8x8_t& g, uint8x8_t& b, uint8x8_t& a);
-using LoadBufA  = void (*)(uint8_t* buf, uint8x8_t& r, uint8x8_t& g, uint8x8_t& b, uint8x8_t& a, uint8_t opa);
-using NeonBlend = void (*)(uint8x8_t& r1, uint8x8_t& g1, uint8x8_t& b1, uint8x8_t& a1,
-                           uint8x8_t r2, uint8x8_t g2, uint8x8_t b2, uint8x8_t a2);
-using StoreBuf  = void (*)(uint8_t* buf, uint8x8_t& r, uint8x8_t& g, uint8x8_t& b, uint8x8_t& a);
+    using LoadBuf = void (*)(uint8_t* buf, uint8x8_t& r, uint8x8_t& g, uint8x8_t& b, uint8x8_t& a);
+    using LoadBufA = void (*)(uint8_t* buf, uint8x8_t& r, uint8x8_t& g, uint8x8_t& b, uint8x8_t& a, uint8_t opa);
+    using NeonBlend = void (*)(uint8x8_t& r1, uint8x8_t& g1, uint8x8_t& b1, uint8x8_t& a1,
+                               uint8x8_t r2, uint8x8_t g2, uint8x8_t b2, uint8x8_t a2);
+    using StoreBuf = void (*)(uint8_t* buf, uint8x8_t& r, uint8x8_t& g, uint8x8_t& b, uint8x8_t& a);
 
-struct {
-    ColorMode dm;
-    LoadBuf loadDstFunc;
-    NeonBlend blendFunc;
-    StoreBuf storeDstFunc;
-} g_dstFunc[] = {
-    {ARGB8888, LoadBuf_ARGB8888, NeonBlendRGBA, StoreBuf_ARGB8888},
-    {RGB888,   LoadBuf_RGB888,   NeonBlendRGB,  StoreBuf_RGB888},
-    {RGB565,   LoadBuf_RGB565,   NeonBlendRGB,  StoreBuf_RGB565}
-};
+    struct {
+        ColorMode dm;
+        LoadBuf loadDstFunc;
+        NeonBlend blendFunc;
+        StoreBuf storeDstFunc;
+    } g_dstFunc[] = {
+        {ARGB8888, LoadBuf_ARGB8888, NeonBlendRGBA, StoreBuf_ARGB8888},
+        {RGB888, LoadBuf_RGB888, NeonBlendRGB, StoreBuf_RGB888},
+        {RGB565, LoadBuf_RGB565, NeonBlendRGB, StoreBuf_RGB565}};
 
-struct {
-    ColorMode sm;
-    LoadBufA loadSrcFunc;
-} g_srcFunc[] = {
-    {ARGB8888, LoadBufA_ARGB8888},
-    {RGB888,   LoadBufA_RGB888},
-    {RGB565,   LoadBufA_RGB565}
-};
+    struct {
+        ColorMode sm;
+        LoadBufA loadSrcFunc;
+    } g_srcFunc[] = {
+        {ARGB8888, LoadBufA_ARGB8888},
+        {RGB888, LoadBufA_RGB888},
+        {RGB565, LoadBufA_RGB565}};
 
-class NeonBlendPipeLine {
-public:
-    NeonBlendPipeLine() {}
-    ~NeonBlendPipeLine() {}
+    class NeonBlendPipeLine {
+    public:
+        NeonBlendPipeLine()
+        {}
+        ~NeonBlendPipeLine()
+        {}
 
-    void Construct(ColorMode dm, ColorMode sm, void* srcColor = nullptr, uint8_t opa = OPA_OPAQUE)
-    {
-        int16_t dstNum = sizeof(g_dstFunc) / sizeof(g_dstFunc[0]);
-        for (int16_t i = 0; i < dstNum; ++i) {
-            if (g_dstFunc[i].dm == dm) {
-                loadDstFunc_ = g_dstFunc[i].loadDstFunc;
-                blendFunc_ = g_dstFunc[i].blendFunc;
-                storeDstFunc_ = g_dstFunc[i].storeDstFunc;
-                break;
+        void Construct(ColorMode dm, ColorMode sm, void* srcColor = nullptr, uint8_t opa = OPA_OPAQUE)
+        {
+            int16_t dstNum = sizeof(g_dstFunc) / sizeof(g_dstFunc[0]);
+            for (int16_t i = 0; i < dstNum; ++i) {
+                if (g_dstFunc[i].dm == dm) {
+                    loadDstFunc_ = g_dstFunc[i].loadDstFunc;
+                    blendFunc_ = g_dstFunc[i].blendFunc;
+                    storeDstFunc_ = g_dstFunc[i].storeDstFunc;
+                    break;
+                }
+            }
+            int16_t srcNum = sizeof(g_srcFunc) / sizeof(g_srcFunc[0]);
+            for (int16_t i = 0; i < srcNum; ++i) {
+                if (g_srcFunc[i].sm == sm) {
+                    loadSrcFunc_ = g_srcFunc[i].loadSrcFunc;
+                    break;
+                }
+            }
+            if (srcColor != nullptr) {
+                ConstructSrcColor(sm, srcColor, opa, r2_, g2_, b2_, a2_);
             }
         }
-        int16_t srcNum = sizeof(g_srcFunc) / sizeof(g_srcFunc[0]);
-        for (int16_t i = 0; i < srcNum; ++i) {
-            if (g_srcFunc[i].sm == sm) {
-                loadSrcFunc_ = g_srcFunc[i].loadSrcFunc;
-                break;
+
+        void Invoke(uint8_t* dst, uint8_t* src, uint8_t opa)
+        {
+            loadDstFunc_(dst, r1_, g1_, b1_, a1_);
+            loadSrcFunc_(src, r2_, g2_, b2_, a2_, opa);
+            blendFunc_(r1_, g1_, b1_, a1_, r2_, g2_, b2_, a2_);
+            storeDstFunc_(dst, r1_, g1_, b1_, a1_);
+        }
+
+        void Invoke(uint8_t* dst)
+        {
+            loadDstFunc_(dst, r1_, g1_, b1_, a1_);
+            blendFunc_(r1_, g1_, b1_, a1_, r2_, g2_, b2_, a2_);
+            storeDstFunc_(dst, r1_, g1_, b1_, a1_);
+        }
+
+        void Invoke(uint8_t* dst, uint8x8_t& r, uint8x8_t& g, uint8x8_t& b, uint8x8_t& a)
+        {
+            loadDstFunc_(dst, r1_, g1_, b1_, a1_);
+            blendFunc_(r1_, g1_, b1_, a1_, r, g, b, a);
+            storeDstFunc_(dst, r1_, g1_, b1_, a1_);
+        }
+
+        static void inline void NeonPrelerp_ARGB8888(uint8_t* buf, uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha)
+        {
+            uint8x8x4_t vBuf = vld4_u8(buf);
+            uint8x8_t r0 = vBuf.val[NEON_R];
+            uint8x8_t g0 = vBuf.val[NEON_G];
+            uint8x8_t b0 = vBuf.val[NEON_B];
+            uint8x8_t a0 = vBuf.val[NEON_A];
+
+            uint8x8_t r1 = vdup_n_u8(red);
+            uint8x8_t g1 = vdup_n_u8(green);
+            uint8x8_t b1 = vdup_n_u8(blue);
+            uint8x8_t a1 = vdup_n_u8(alpha);
+
+            uint8x8_t rs = NeonPreLerp(r0, r1, a1);
+            uint8x8_t gs = NeonPreLerp(g0, g1, a1);
+            uint8x8_t bs = NeonPreLerp(b0, b1, a1);
+            uint8x8_t as = NeonPreLerp(a0, a1, a1);
+
+            StoreBuf_ARGB8888(vBuf, rs, gs, bs, as);
+        }
+        static void inline void NeonPrelerp_ARGB8888(uint8_t* buf, uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha, uint8_t cover)
+        {
+            uint8x8x4_t vBuf = vld4_u8(buf);
+            uint8x8_t r0 = vBuf.val[NEON_R];
+            uint8x8_t g0 = vBuf.val[NEON_G];
+            uint8x8_t b0 = vBuf.val[NEON_B];
+            uint8x8_t a0 = vBuf.val[NEON_A];
+
+            uint8x8_t r1 = Multiply(vdup_n_u8(red), cover);
+            uint8x8_t g1 = Multiply(vdup_n_u8(green), cover);
+            uint8x8_t b1 = Multiply(vdup_n_u8(blue), cover);
+            uint8x8_t a1 = Multiply(vdup_n_u8(alpha), cover);
+
+            uint8x8_t rs = NeonPreLerp(r0, r1, a1);
+            uint8x8_t gs = NeonPreLerp(g0, g1, a1);
+            uint8x8_t bs = NeonPreLerp(b0, b1, a1);
+            uint8x8_t as = NeonPreLerp(a0, a1, a1);
+
+            StoreBuf_ARGB8888(vBuf, rs, gs, bs, as);
+        }
+        static void inline void NeonLerp_ARGB8888(uint8_t* buf, uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+        {
+            uint8x8x4_t vBuf = vld4_u8(buf);
+            uint8x8_t r0 = vBuf.val[NEON_R];
+            uint8x8_t g0 = vBuf.val[NEON_G];
+            uint8x8_t b0 = vBuf.val[NEON_B];
+            uint8x8_t a0 = vBuf.val[NEON_A];
+
+            uint8x8_t r1 = vdup_n_u8(r);
+            uint8x8_t g1 = vdup_n_u8(g);
+            uint8x8_t b1 = vdup_n_u8(b);
+            uint8x8_t a1 = vdup_n_u8(a);
+
+            uint8x8_t rs = NeonLerp(r0, r1, a1);
+            uint8x8_t gs = NeonLerp(g0, g1, a1);
+            uint8x8_t bs = NeonLerp(b0, b1, a1);
+            uint8x8_t as = NeonPreLerp(a0, a1, a1);
+
+            StoreBuf_ARGB8888(vBuf, rs, gs, bs, as);
+        }
+        static void inline void NeonLerp_ARGB8888(uint8_t* buf, uint8_t r, uint8_t g, uint8_t b, uint8_t a, uint8_t cover)
+        {
+            uint8x8x4_t vBuf = vld4_u8(buf);
+            uint8x8_t r0 = vBuf.val[NEON_R];
+            uint8x8_t g0 = vBuf.val[NEON_G];
+            uint8x8_t b0 = vBuf.val[NEON_B];
+            uint8x8_t a0 = vBuf.val[NEON_A];
+
+            uint8x8_t r1 = Multiply(vdup_n_u8(r), cover);
+            uint8x8_t g1 = Multiply(vdup_n_u8(g), cover);
+            uint8x8_t b1 = Multiply(vdup_n_u8(b), cover);
+            uint8x8_t a1 = Multiply(vdup_n_u8(a), cover);
+
+            uint8x8_t rs = NeonLerp(r0, r1, a1);
+            uint8x8_t gs = NeonLerp(g0, g1, a1);
+            uint8x8_t bs = NeonLerp(b0, b1, a1);
+            uint8x8_t as = NeonPreLerp(a0, a1, a1);
+
+            StoreBuf_ARGB8888(vBuf, rs, gs, bs, as);
+        }
+
+    private:
+        void ConstructSrcColor(ColorMode sm, void* srcColor, uint8_t opa,
+                               uint8x8_t& r, uint8x8_t& g, uint8x8_t& b, uint8x8_t& a)
+        {
+            if (sm == ARGB8888) {
+                Color32* color = reinterpret_cast<Color32*>(srcColor);
+                r = vdup_n_u8(color->red);
+                g = vdup_n_u8(color->green);
+                b = vdup_n_u8(color->blue);
+                a = NeonMulDiv255(vdup_n_u8(opa), vdup_n_u8(color->alpha));
+            } else if (sm == RGB888) {
+                Color24* color = reinterpret_cast<Color24*>(srcColor);
+                r = vdup_n_u8(color->red);
+                g = vdup_n_u8(color->green);
+                b = vdup_n_u8(color->blue);
+                a = vdup_n_u8(opa);
+            } else if (sm == RGB565) {
+                Color16* color = reinterpret_cast<Color16*>(srcColor);
+                r = vdup_n_u8(color->red);
+                g = vdup_n_u8(color->green);
+                b = vdup_n_u8(color->blue);
+                a = vdup_n_u8(opa);
             }
         }
-        if (srcColor != nullptr) {
-            ConstructSrcColor(sm, srcColor, opa, r2_, g2_, b2_, a2_);
-        }
-    }
 
-    void Invoke(uint8_t* dst, uint8_t* src, uint8_t opa)
-    {
-        loadDstFunc_(dst, r1_, g1_, b1_, a1_);
-        loadSrcFunc_(src, r2_, g2_, b2_, a2_, opa);
-        blendFunc_(r1_, g1_, b1_, a1_, r2_, g2_, b2_, a2_);
-        storeDstFunc_(dst, r1_, g1_, b1_, a1_);
-    }
-
-    void Invoke(uint8_t* dst)
-    {
-        loadDstFunc_(dst, r1_, g1_, b1_, a1_);
-        blendFunc_(r1_, g1_, b1_, a1_, r2_, g2_, b2_, a2_);
-        storeDstFunc_(dst, r1_, g1_, b1_, a1_);
-    }
-
-    void Invoke(uint8_t* dst, uint8x8_t& r, uint8x8_t& g, uint8x8_t& b, uint8x8_t& a)
-    {
-        loadDstFunc_(dst, r1_, g1_, b1_, a1_);
-        blendFunc_(r1_, g1_, b1_, a1_, r, g, b, a);
-        storeDstFunc_(dst, r1_, g1_, b1_, a1_);
-    }
-
-    void inline void NeonPPPP_ARGB8888(uint8_t* buf, uint8_t* srcBuf)
-    {
-        uint8x8x4_t vBuf = vld4_u8(buf);
-        uint8x8x4_t vSrcBuf = vld4_u8(srcBuf);
-        uint8x8_t r0 = vBuf.val[NEON_R];
-        uint8x8_t g0 = vBuf.val[NEON_G];
-        uint8x8_t b0 = vBuf.val[NEON_B];
-        uint8x8_t a0 = vBuf.val[NEON_A];
-
-        uint8x8_t r1 =vSrcBuf.val[NEON_R];
-        uint8x8_t g1= vSrcBuf.val[NEON_R];;
-        uint8x8_t b1= vSrcBuf.val[NEON_R];
-        uint8x8_t a1= vSrcBuf.val[NEON_R];
-
-        uint8x8_t rs = NeonPreLerp(r0,r1,a1);
-        uint8x8_t gs = NeonPreLerp(g0,g1,a1);
-        uint8x8_t bs = NeonPreLerp(b0,b1,a1);
-        uint8x8_t as = NeonPreLerp(a0,a1,a1);
-
-        StoreBuf_ARGB8888(vBuf,rs,gs,bs,as);
-    }
-
-    void inline void NeonLLLP_ARGB8888(uint8_t* buf, uint8_t r, uint8_t g, uint8_t b, uint8_t a)
-    {
-        uint8x8x4_t vBuf = vld4_u8(buf);
-        uint8x8_t r0 = vBuf.val[NEON_R];
-        uint8x8_t g0 = vBuf.val[NEON_G];
-        uint8x8_t b0 = vBuf.val[NEON_B];
-        uint8x8_t a0 = vBuf.val[NEON_A];
-
-        uint8x8_t r1 =vdup_n_u8(r);
-        uint8x8_t g1= vdup_n_u8(g);
-        uint8x8_t b1= vdup_n_u8(b);
-        uint8x8_t a1= vdup_n_u8(a);
-
-        uint8x8_t rs = NeonLerp(r0,r1,a1);
-        uint8x8_t gs = NeonLerp(g0,g1,a1);
-        uint8x8_t bs = NeonLerp(b0,b1,a1);
-        uint8x8_t as = NeonPreLerp(a0,a1,a1);
-
-        StoreBuf_ARGB8888(vBuf,rs,gs,bs,as);
-    }
-
-private:
-    void ConstructSrcColor(ColorMode sm, void* srcColor, uint8_t opa,
-                           uint8x8_t& r, uint8x8_t& g, uint8x8_t& b, uint8x8_t& a)
-    {
-        if (sm == ARGB8888) {
-            Color32* color = reinterpret_cast<Color32*>(srcColor);
-            r = vdup_n_u8(color->red);
-            g = vdup_n_u8(color->green);
-            b = vdup_n_u8(color->blue);
-            a = NeonMulDiv255(vdup_n_u8(opa), vdup_n_u8(color->alpha));
-        } else if (sm == RGB888) {
-            Color24* color = reinterpret_cast<Color24*>(srcColor);
-            r = vdup_n_u8(color->red);
-            g = vdup_n_u8(color->green);
-            b = vdup_n_u8(color->blue);
-            a = vdup_n_u8(opa);
-        } else if (sm == RGB565) {
-            Color16* color = reinterpret_cast<Color16*>(srcColor);
-            r = vdup_n_u8(color->red);
-            g = vdup_n_u8(color->green);
-            b = vdup_n_u8(color->blue);
-            a = vdup_n_u8(opa);
-        }
-    }
-
-    LoadBuf loadDstFunc_ = nullptr;
-    LoadBufA loadSrcFunc_ = nullptr;
-    NeonBlend blendFunc_ = nullptr;
-    StoreBuf storeDstFunc_ = nullptr;
-    uint8x8_t r1_;
-    uint8x8_t g1_;
-    uint8x8_t b1_;
-    uint8x8_t a1_;
-    uint8x8_t r2_;
-    uint8x8_t g2_;
-    uint8x8_t b2_;
-    uint8x8_t a2_;
-};
-}
+        LoadBuf loadDstFunc_ = nullptr;
+        LoadBufA loadSrcFunc_ = nullptr;
+        NeonBlend blendFunc_ = nullptr;
+        StoreBuf storeDstFunc_ = nullptr;
+        uint8x8_t r1_;
+        uint8x8_t g1_;
+        uint8x8_t b1_;
+        uint8x8_t a1_;
+        uint8x8_t r2_;
+        uint8x8_t g2_;
+        uint8x8_t b2_;
+        uint8x8_t a2_;
+    };
+} // namespace OHOS
 #endif
 #endif
